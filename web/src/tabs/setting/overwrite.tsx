049d3f6b8eaf0673d85440067b64e56b56e0f878
qxdjz <16165437+qxdjz@users.noreply.github.com>
2025-07-18 18:34:08 +0800

    fix

--- web/src/tabs/setting/overwrite.tsx
+++ web/src/tabs/setting/overwrite.tsx
@@ -71,13 +71,6 @@ export const OverWriteSetting = () => {
                         rules={[{ pattern: new RegExp(/0|^[1-9][0-9]{3}$/) }]}>
                         <Input placeholder="请输入HTTP(S)&SOCKS5 混合代理端" />
                     </Form.Item>
-                    <Form.Item
-                        name="nameserver-policy"
-                        label="nameserver-policy"
-                        help="nameserver-policy"
-                        initialValue={data['nameserver-policy']}>
-                        <TextArea placeholder="请输入自定义域名解析dns服务器" />
-                    </Form.Item>
                 </Form>
             )}
         </>
